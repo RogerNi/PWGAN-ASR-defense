0f86d0b101cf67c16c671ec28977ed66581aba65
Ronghao Ni <nironghao@gmail.com>
2022-12-04 00:27:48 -0500

    Ignore datasets

--- online_defense/PWGAN_defense.py
+++ online_defense/PWGAN_defense.py
@@ -1,24 +1,27 @@
-from torchaudio.functional import Resample
+from torchaudio.functional import resample
 from functools import partial
 import torch
 from utils import logmelfilterbank_torch
 from parallel_wavegan.utils import load_model
 import os, tempfile
 from parallel_wavegan.utils import download_pretrained_model
+import yaml
 
 
 class PWGAN_Defense(torch.nn.Module):
     def __init__(self, config, ckpt_path):
         super().__init__()
-        self.pre_resample = Resample(16000, 24000)
-        self.post_resample = Resample(24000, 16000)
+        self.pre_resample = partial(resample, orig_freq=16000, new_freq=24000)
+        self.post_resample = partial(resample, orig_freq=24000, new_freq=16000)
+        self.window_tensor = torch.hann_window(config["win_length"])
+        self.config = config
         self.log_mel_feature = partial(
             logmelfilterbank_torch,             
             sampling_rate=config["sampling_rate"],
             hop_size=config["hop_size"],
             fft_size=config["fft_size"],
             win_length=config["win_length"],
-            window=config["window"],
+            window=self.window_tensor,
             num_mels=config["num_mels"],
             fmin=config["fmin"],
             fmax=config["fmax"],
@@ -27,19 +30,41 @@ class PWGAN_Defense(torch.nn.Module):
         model = load_model(ckpt_path, config)
         model.remove_weight_norm()
         model = model.eval()
-        self.pwgan = model
+        self.model = model
         
     def forward(self, x):
         x = self.pre_resample(x)
         x = self.log_mel_feature(x)
         x = self.model.inference(x, normalize_before=True).view(-1)
         return self.post_resample(x)
+    
+    def to(self, device):
+        new_self = super(PWGAN_Defense, self).to(device)
+        self.window_tensor = self.window_tensor.to(device)
+        config = self.config
+        self.log_mel_feature = partial(
+            logmelfilterbank_torch,             
+            sampling_rate=config["sampling_rate"],
+            hop_size=config["hop_size"],
+            fft_size=config["fft_size"],
+            win_length=config["win_length"],
+            window=self.window_tensor,
+            num_mels=config["num_mels"],
+            fmin=config["fmin"],
+            fmax=config["fmax"],
+            log_base=config.get("log_base", 10.0),
+            device=device
+        )
+
+        return new_self
         
         
 class PWGAN_Defense_auto_config(PWGAN_Defense):
     def __init__(self, ckpt_path):
         dirname = os.path.dirname(ckpt_path)
         config = os.path.join(dirname, "config.yml")
+        with open(config) as f:
+            config = yaml.load(f, Loader=yaml.Loader)
         super().__init__(config, ckpt_path)
         
 class PWGAN_Defense_auto_ckpt(PWGAN_Defense_auto_config):
@@ -47,7 +72,7 @@ class PWGAN_Defense_auto_ckpt(PWGAN_Defense_auto_config):
         tmp_dir = os.path.join(tempfile.gettempdir(), "wpgan_tmp")
         try:
             os.makedirs(tmp_dir)
-            download_pretrained_model("1zHQl8kUYEuZ_i1qEFU6g2MEu99k3sHmR", tmp_dir)
-        except:
+            download_pretrained_model("libritts_parallel_wavegan.v1", tmp_dir)
+        except Exception as e:
             pass
-        super().__init__(os.path.join(tmp_dir, "checkpoint-400000steps.pkl"))
+        super().__init__(os.path.join(tmp_dir, "libritts_parallel_wavegan.v1", "checkpoint-400000steps.pkl"))
